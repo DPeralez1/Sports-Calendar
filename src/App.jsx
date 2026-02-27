8efd813ada06c6d8a7753298c074f29636442017
David Peralez <davidperalez89@gmail.com>
2026-02-26 18:46:20 -0800

    updated colors and fonts. Not happy will update again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 import Calendar from "./components/Calendar";
+<link
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+  rel="stylesheet"
+></link>;
 
 function App() {
   return (
